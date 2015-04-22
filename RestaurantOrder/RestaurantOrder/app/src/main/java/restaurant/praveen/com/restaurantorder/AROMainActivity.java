0772001b3ed389721f40e367ada947ddfccd035c
Praveen VK <roboandroidapp@gmail.com>
2015-04-22 11:27:15 +0530

    navigation drawer added

--- RestaurantOrder/RestaurantOrder/app/src/main/java/restaurant/praveen/com/restaurantorder/AROMainActivity.java
+++ RestaurantOrder/RestaurantOrder/app/src/main/java/restaurant/praveen/com/restaurantorder/AROMainActivity.java
@@ -2,11 +2,17 @@ package restaurant.praveen.com.restaurantorder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ListView;
 
 import restaurant.praveen.com.restaurantorder.entity.CROItem;
 import restaurant.praveen.com.restaurantorder.entity.CROOrderSummary;
@@ -15,10 +21,47 @@ import restaurant.praveen.com.restaurantorder.utils.CROConstants;
 
 public class AROMainActivity extends ActionBarActivity implements View.OnClickListener {
 
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String[] mPlanetTitles;
+    private ListView mDrawerList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.content_frame);
+        frameLayout.addView(getLayoutInflater().inflate(
+                R.layout.layout_main, null));
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.mipmap.ic_launcher, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle("Title");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                ActionBar actionBar = getSupportActionBar();
+                actionBar.setTitle("DrawerTitle");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
         init();
     }
 
